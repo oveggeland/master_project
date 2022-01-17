3007546810aed56238d9edd1b7a2dc439d95ed21
Oskar Veggeland <oveggeland@gmail.com>
2022-01-17 17:38:53 +0100

    ros_openimu driver to save time stamps for cameras

--- drivers/src/ros_openimu/scripts/openimu_driver.py
+++ drivers/src/ros_openimu/scripts/openimu_driver.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
 import rospy
+from synchronizer.srv import *
 import sys
 import math
 from time import time
-from sensor_msgs.msg import Imu, MagneticField
+from sensor_msgs.msg import Imu
 
 try:
     from ros_openimu.src.aceinna.tools import OpenIMU
@@ -31,34 +32,52 @@ class OpenIMUros:
     '''
 
     def readimu(self):
-        readback = self.openimudev.getdata('z1')
+        readback = self.openimudev.getdata('o1')
         return readback
 
 if __name__ == "__main__":
     rospy.init_node("openimu_driver")
 
-    pub_imu = rospy.Publisher('imu_acc_ar', Imu, queue_size=1)
-    #pub_mag = rospy.Publisher('imu_mag', MagneticField, queue_size=1)
-
+    pub_imu = rospy.Publisher('imu_acc_ar', Imu, queue_size=5)
     imu_msg = Imu()             # IMU data
-    #mag_msg = MagneticField()   # Magnetometer data
-    
-    rate = rospy.Rate(200)   # 200Hz
+        
+    rate = 200  # 200Hz
+    clk = rospy.Rate(rate)   
     seq = 0
     frame_id = 'OpenIMU'
     convert_rads = math.pi /180
-    convert_tesla = 1/10000
 
     openimu_wrp = OpenIMUros()
     rospy.loginfo("OpenIMU driver initialized.")
 
+    # Persistent time stamp server proxy
+    rospy.wait_for_service('set_time_stamp')
+    set_time_stamp = rospy.ServiceProxy('set_time_stamp', SetTimeStamp, persistent=True)
+
     while not rospy.is_shutdown():
         #read the data - call the get imu measurement data
         readback = openimu_wrp.readimu()
-        #publish the data m/s^2 and convert deg/s to rad/s
-        imu_msg.header.stamp = rospy.Time.now()
+
+        # Get time stamp from the evaluation board
+        us = readback[0]
+        ts = rospy.Time(0, us*1000)
+
+        # Check if time stamp should be shared with cameras
+        cam_stamp = readback[7]
+        if cam_stamp:
+            print("Cam stamp! Storing to server:", str(ts.secs+ts.nsecs/10**9))
+            # Request to store on server
+            try:
+                resp = set_time_stamp(ts.secs, ts.nsecs)
+            except rospy.ServiceException as exc:
+                print("Service did not process request: " + str(exc))
+
+    
+        # Create IMU topic message
+        imu_msg.header.stamp = ts
         imu_msg.header.frame_id = frame_id
         imu_msg.header.seq = seq
+
         imu_msg.orientation_covariance[0] = -1
         imu_msg.linear_acceleration.x = readback[1]
         imu_msg.linear_acceleration.y = readback[2]
@@ -68,20 +87,11 @@ if __name__ == "__main__":
         imu_msg.angular_velocity.y = readback[5] * convert_rads
         imu_msg.angular_velocity.z = readback[6] * convert_rads
         imu_msg.angular_velocity_covariance[0] = -1
+
         pub_imu.publish(imu_msg)
 
-        # Publish magnetometer data - convert Gauss to Tesla
-        #mag_msg.header.stamp = imu_msg.header.stamp
-        #mag_msg.header.frame_id = frame_id
-        #mag_msg.header.seq = seq
-        #mag_msg.magnetic_field.x = readback[7] * convert_tesla
-        #mag_msg.magnetic_field.y = readback[8] * convert_tesla
-        #mag_msg.magnetic_field.z = readback[9] * convert_tesla
-        #mag_msg.magnetic_field_covariance = [0,0,0,0,0,0,0,0,0]
-        #pub_mag.publish(mag_msg)
-
         seq = seq + 1
-        rate.sleep()
+        #clk.sleep()
     openimu_wrp.close()         # exit
 
 
