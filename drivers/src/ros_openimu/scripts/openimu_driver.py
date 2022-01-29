a3c7fa219b0ddee04684f858dd11a23a9e19d0ff
Oskar Veggeland <oveggeland@gmail.com>
2022-01-29 16:52:15 +0100

    openimu driver waits for cameras to be ready before calling IMU triggering

--- drivers/src/ros_openimu/scripts/openimu_driver.py
+++ drivers/src/ros_openimu/scripts/openimu_driver.py
@@ -4,7 +4,7 @@ import rospy
 from synchronizer.srv import *
 import sys
 import math
-from time import time
+from time import sleep, time
 from sensor_msgs.msg import Imu
 import random
 
@@ -36,47 +36,59 @@ class OpenIMUros:
         readback = self.openimudev.getdata('o1')
         return readback
 
+
 if __name__ == "__main__":
     rospy.init_node("openimu_driver")
 
     pub_imu = rospy.Publisher('imu_acc_ar', Imu, queue_size=5)
     imu_msg = Imu()             # IMU data
         
-    # rate = 200  # 200Hz
-    # clk = rospy.Rate(rate)   
     seq = 0
     frame_id = 'OpenIMU'
     convert_rads = math.pi /180
 
     options = {'baudrate':230400, 'filter_device_type':'IMU', 'com_port':'auto'}
+    #options = {'baudrate':'auto', 'filter_device_type':'auto', 'com_port':'auto'}
     openimu_wrp = OpenIMUros(options)
     rospy.loginfo("OpenIMU driver initialized.")
 
-    # Persistent time stamp server proxy
-    rospy.wait_for_service('set_time_stamp')
-    set_time_stamp = rospy.ServiceProxy('set_time_stamp', SetTimeStamp, persistent=True)
+
     cam_count = 0
 
-    prev = rospy.Time(0,0)
+
+    # Wait for cameras to be ready!
+    rospy.wait_for_service('ready_request')    
+    cams_ready = rospy.ServiceProxy('ready_request', ReadyRequest)
+    while not cams_ready().ready:
+        continue
+
+    print("Cameras are ready! Inform IMU to start triggering procedure!")
+    while openimu_wrp.openimudev.set_cam_ready_flag(0)['packetType'] != 'success':
+        continue
+    # openimu_wrp.openimudev.setpacketrate(200)
+
+    # Persistent time stamp server proxy
+    rospy.wait_for_service('set_time_stamp')    
+    set_time_stamp = rospy.ServiceProxy('set_time_stamp', SetTimeStamp, persistent=True)
+
+
     while not rospy.is_shutdown():
         #read the data - call the get imu measurement data
-
-
         readback = openimu_wrp.readimu()
+       
         if readback:
-
+            
             # Get time stamp from the evaluation board
             us = readback[0]
             ts = rospy.Time(0, us*1000)
 
             # Check if time stamp should be shared with cameras
-            cam_stamp = readback[7]
+            cam_stamp = readback[8]
             if cam_stamp:
                 print("Cam stamp! Storing to server:", str(ts.secs+ts.nsecs/10**9))
                 # Request to store on server
                 try:
                     resp = set_time_stamp(ts.secs, ts.nsecs, cam_count)
-                    cam_count += 1
                 except rospy.ServiceException as exc:
                     print("Service did not process request: " + str(exc))
 
@@ -99,7 +111,6 @@ if __name__ == "__main__":
 
             seq = seq + 1
 
-            #clk.sleep()
     openimu_wrp.close()         # exit
 
 
