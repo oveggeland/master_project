0435d97cc4f5e2b1e0d743f05a3f722c175659ba
Oskar Veggeland <oveggeland@gmail.com>
2022-04-28 17:36:51 +0200

    change to individual config folders for all baselines

--- rovio/build_rovio.bash
+++ rovio/build_rovio.bash
@@ -1,1 +1,6 @@
-catkin build rovio --cmake-args -DCMAKE_BUILD_TYPE=Release -DROVIO_NCAM=2
+if [ -z "$1" ]
+then
+	catkin build rovio --cmake-args -DCMAKE_BUILD_TYPE=Release -DROVIO_NCAM=2
+else
+	catkin build rovio --cmake-args -DCMAKE_BUILD_TYPE=Release -DROVIO_NCAM=$1
+fi
